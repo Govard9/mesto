97c922af46e1b5b6a36fb178eea3e10ec9dbccb5
govard9 <web-supsu@yandex.ru>
2022-11-29 13:08:18 +0700

    fix: сгруппировать все переменные наверх файла js, а слушатели вниз. Исправить ошибку при изменении профиля.

--- script/index.js
+++ script/index.js
@@ -1,41 +1,26 @@
 // Открытие popup
-
 let buttonEditProfile = document.querySelector(".profile__edit-button");
+
+// Закрытие popup
+let buttonClosePopUp = document.querySelector(".popup__close");
+
+// Добавление active класса для кнопки like
+let btnAll = document.querySelectorAll(".photo__like");
+
+// Сохранение информации в попап
+let formElement = document.querySelector(".popup__main-container");
+let nameInput = formElement.querySelector(".popup__item_data_name");
+let jobInput = formElement.querySelector(".popup__item_data_profession");
 
 function listenBtnEditProfile() {
   let popup = document.querySelector(".popup");
   popup.classList.add("popup_opened");
 }
 
-buttonEditProfile.addEventListener("click", listenBtnEditProfile);
-
-// Закрытие popup
-
-let buttonClosePopUp = document.querySelector(".popup__close");
-
 function listenBtnClosePopUp() {
   let hidePopUp = document.querySelector(".popup");
   hidePopUp.classList.remove("popup_opened");
 }
-
-buttonClosePopUp.addEventListener("click", listenBtnClosePopUp);
-
-// Добавление active класса для кнопки like
-
-let btnAll = document.querySelectorAll(".photo__like");
-
-for (let i = 0; i < btnAll.length; i++) {
-  btnAll[i].addEventListener("click", function () {
-    btnAll[i].classList.add("photo__like_active");
-  });
-}
-
-// Сохранение информации в попап
-
-let formElement = document.querySelector(".popup__container");
-
-let nameInput = formElement.querySelector(".poput__item-author");
-let jobInput = formElement.querySelector(".poput__item-profession");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -43,7 +28,7 @@ function handleFormSubmit(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   // Так мы можем определить свою логику отправки.
   // О том, как это делать, расскажем позже.
-  
+
   // Получите значение полей jobInput и nameInput из свойства value
   let getAtributeName = nameInput.value;
   let getAtributeJob = jobInput.value;
@@ -54,9 +39,19 @@ function handleFormSubmit(evt) {
 
   // Вставьте новые значения с помощью textContent
   profileAuthor.textContent = getAtributeName;
-  profileProfession.textContent = getAtributeJob; 
+  profileProfession.textContent = getAtributeJob;
 }
+
+for (let i = 0; i < btnAll.length; i++) {
+  btnAll[i].addEventListener("click", function () {
+    btnAll[i].classList.add("photo__like_active");
+  });
+}
+
+buttonClosePopUp.addEventListener("click", listenBtnClosePopUp);
+
+buttonEditProfile.addEventListener("click", listenBtnEditProfile);
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener("submit", handleFormSubmit);
