3291845486b863313e06aba297e0a7b877a4ba6f
govard9 <web-supsu@yandex.ru>
2022-12-05 15:12:57 +0700

    feat: добавить массив с данными для карточек

--- script/index.js
+++ script/index.js
@@ -1,3 +1,31 @@
+// массив с данными
+const initialCards = [
+  {
+    name: 'Плотина',
+    link: './images/bobrovaya_plotina.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: './images/dombay.jpg'
+  },
+  {
+    name: 'Эльбрус',
+    link: './images/gora_elbrus.jpg'
+  },
+  {
+    name: 'Церковь',
+    link: './images/karachaevsk.jpg'
+  },
+  {
+    name: 'Пастбища в Китае',
+    link: './images/pastbishe_v_kitae.jpg'
+  },
+  {
+    name: 'Пик Чукаш',
+    link: './images/pik_chukash.jpg'
+  }
+]; 
+
 // Открытие popup
 let buttonEditProfile = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
