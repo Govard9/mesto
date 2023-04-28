c655a9ee8dd0fc6d174eae9a075479f0221993be
Govard9 <web-supsu@yandex.ru>
2023-04-28 14:17:30 +0700

    fix: добавление информации в файл README.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,7 @@ import { PopupWithImage } from '../components/PopupWithImage.js';
 import { PopupWithForm } from '../components/PopupWithForm.js';
 import { PopupDeleteCard } from '../components/PopupDeleteCard.js';
 import { UserInfo } from '../components/UserInfo.js';
-import { Api } from '../components/Api.js';
+import { Api } from '../../../mesto-react/src/utils/Api.js';
 
 let userId;
 let cardsListSection;
